28b5d3efddc4f91c5053edddf230153415415d99
Patricio Torres <ptorresmar@unsa.edu.pe>
2020-07-13 14:15:09 -0500

    Login

--- loginpersonal.cpp
+++ loginpersonal.cpp
@@ -2,6 +2,10 @@
 #include "ui_loginpersonal.h"
 #include "aceptarcarrera.h"
 #include "menuusuario.h"
+#include "QFile"
+#include "QTextStream"
+#include "QDebug"
+
 loginPersonal::loginPersonal(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::loginPersonal)
@@ -16,13 +20,34 @@ loginPersonal::~loginPersonal()
 
 void loginPersonal::on_pushButton_3_clicked()
 {
-    if(ui->usuario->text()=="admin"){
-        hide();
-        MenuUsuario* menuAdmin = new MenuUsuario;
-        menuAdmin->show();
-    }else{
-        hide();
-        aceptarcarrera* menuTaxi = new aceptarcarrera;
-        menuTaxi->show();
+    if(ui->usuario->text().length()==0 && ui->clave->text().length()==0){
+        return;
     }
+    QFile usuario;
+    usuario.setFileName("C:\\Users\\DELL\\Documents\\TrabajoFinalPS\\fonts\\usuarios.txt");
+        if (!usuario.open(QIODevice::ReadOnly | QIODevice::Text))
+                return;
+    QTextStream in(&usuario);
+    while (!in.atEnd()){
+        QString line = in.readLine();
+        if(line.length()==0)
+            break;
+        if(line.mid(0,line.indexOf("-")) == (ui->usuario->text())){
+            if(line.mid(line.indexOf("-")+1,line.length()-line.indexOf("-")-3) == (ui->clave->text())){
+                if(line.mid(line.indexOf("/")+1)=="1"){
+                    hide();
+                    MenuUsuario* menuAdmin = new MenuUsuario;
+                    menuAdmin->show();
+
+                }else{
+                    hide();
+                    aceptarcarrera* menuTaxi = new aceptarcarrera;
+                    menuTaxi->show();
+                }
+            }
+            break;
+        }
+    }
+    usuario.close();
+
 }
